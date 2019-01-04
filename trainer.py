a6417951778057c363e7df7669e0bd76f71d5f71
Jomo Fisher <jomof@google.com>
2019-01-04 11:53:40 -0800

    a

--- trainer.py
+++ trainer.py
@@ -1,6 +1,29 @@
-import numpy as np
-from sklearn.model_selection import train_test_split
+# TensorFlow and tf.keras
+import tensorflow as tf
 from tensorflow import keras
+import seaborn as sns
+from keras.models import load_model
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+from IPython.display import clear_output
+import imageio
+from IPython.display import Image
+from IPython.display import HTML
+from scipy.special import logit
+from scipy.special import expit
+from sklearn.preprocessing import scale
+from os import walk
+import os
+import matplotlib.pyplot as plt
+
+from sklearn.model_selection import train_test_split
+
+print(tf.__version__)
+
+
+from numpy import genfromtxt
 
 def pad_zeros(width, x):
   return np.array(list(x) + [0] * (width - len(x)))
@@ -30,7 +53,7 @@ def array_sign_interleave(x):
 def remove_nans(a):
   return [x for x in a if str(x) != 'nan']
 
-dataCsv = np.genfromtxt('data.csv', delimiter=',')
+dataCsv = genfromtxt('data.csv', delimiter=',')
 
 outputs, residue = np.hsplit(dataCsv, [1])
 ratios, times = np.hsplit(residue, [1])
@@ -73,18 +96,136 @@ def expand_times(f):
     return array_sign_interleave(pad_zeros(timeWindows, short))
   return array_map(ff, times)
 
+def color_of_answer(answer):
+  if (answer > 0.5): return 'blue'
+  else: return 'gray'
+
 forwardTimes = imbed_reverse_expand_times(identity_transform)
+#reverseTimes = expand_times(reverse_transform)
+#descendingTimes = expand_times(descending_transform)
+#ascendingTimes = expand_times(ascending_transform)
+
+#loss: 0.2310 - binary_accuracy: 0.8976
+#my_infile = np.column_stack((ratios, forwardTimes, reverseTimes, descendingTimes, ascendingTimes))
+#loss: 0.2180 - binary_accuracy: 0.9023 
+#my_infile = np.column_stack((ratios, forwardTimes, reverseTimes))
 my_infile = np.column_stack((ratios, forwardTimes))
+#my_infile = ratios
 my_outfile = outputs
 x_train, x_test, y_train, y_test = train_test_split(my_infile, my_outfile, test_size=0.01)
 x_train.shape, y_train.shape, x_test.shape, y_test.shape
 
-
+model_file = "120-1.h5"
+inputLayer = keras.layers.Dense(120, input_shape=(61,), kernel_initializer='normal', activation='sigmoid')
 my_model = keras.Sequential([
-    keras.layers.Dense(61, input_shape=(61,), kernel_initializer='normal', activation='sigmoid'),
+    inputLayer,
     keras.layers.Dense(1, kernel_initializer='normal', activation='sigmoid')
 ])
 
 my_model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['binary_accuracy'])
+total_epochs = 0
+total_steps = 0
+x_disp = x_test
+y_disp = y_test
+tpy = np.transpose(y_disp)[0]
+c_disp = array_map(color_of_answer, tpy)
 
-my_model.fit(x_train, y_train, epochs=1)
+noise1 = np.random.normal(0, 0.5, y_disp.shape[0])
+noise2 = np.random.normal(0, 0.5, y_disp.shape[0])
+
+
+
+for filename in sorted(os.listdir(".")):
+  if (model_file in filename and ".png" in filename):
+    os.remove(filename)
+    
+    
+class TrainState:
+  total_steps = 0
+  prior_loss = 1.00
+  latest_steps = 1
+  latest_loss = 1.00
+
+state = TrainState()
+
+goal_loss = 0.7
+final_goal_loss = 0.2
+loss_step = -0.005
+render_countdown = 5
+
+
+
+gif_file = "{}.gif".format(model_file)
+
+def train_steps(steps, state):
+  if (steps < state.latest_steps): steps = state.latest_steps
+  state.prior_loss = state.latest_loss
+  #print("Stepping {} starting at {}".format(steps, state.total_steps))
+  my_model.fit(x_train, y_train, epochs=1, steps_per_epoch=steps, verbose = 0)
+  loss, acc = my_model.evaluate(x_test, y_test, verbose=0)
+  print("- step {0} loss = {1:.4f} accuracy = {2:.4f}".format(state.total_steps, loss, acc))
+  state.latest_loss = loss
+  state.total_steps = state.total_steps + steps
+  return state
+
+def train_until(target_loss, state):
+  print("{0}: Seeking target loss {1:.4f}".format(render_countdown, target_loss))
+  while True: 
+    target_loss_delta = 0.7 - state.latest_loss
+    estimated_loss_per_step = target_loss_delta / (1 + state.total_steps)
+
+    if (estimated_loss_per_step > 0):
+      steps_needed = int((state.latest_loss - target_loss) / estimated_loss_per_step)
+      train_steps(steps_needed, state)                              
+    else:
+      train_steps(state.latest_steps, state)   
+      
+    if (target_loss > state.latest_loss): 
+      print("- met loss goal of {0:.4f} at step {1} with actual loss {0:.4f}".format(target_loss, state.total_steps, state.latest_loss))
+      return state
+        
+def save_image(state):
+    predictions = my_model.predict(x_disp)
+    loss, acc = my_model.evaluate(x_disp, y_disp, verbose=0)
+
+    tpp = np.transpose(predictions)[0]
+    n1 = noise1 * loss
+    n2 = noise2 * loss
+    sns.set_style("dark")
+    plt.clf()
+    plt.ylim(-0.5, 1.5)
+    plt.xlim(-0.5, 1.5)
+    title = "{0}: Step {1} loss={2:.4f} accuracy={3:.4f}".format(model_file, state.total_steps, loss, acc)
+    plt.title(title)
+
+    xpn = tpp + n1
+    ypn = 0.5 + n2
+
+    xpn1 = xpn[tpy > 0.5]
+    xpn0 = xpn[tpy < 0.5]
+
+    ypn1 = ypn[tpy > 0.5]
+    ypn0 = ypn[tpy < 0.5]
+
+
+    #sns.kdeplot(xpn, ypn, shade=False, shade_lowest=False, legend=False, cmap="Blues").set_title(title)
+    plt.scatter(xpn, ypn, color=c_disp)
+    #plt.scatter(xpn0, ypn0, color='grey')
+    #sns.kdeplot(xpn0, ypn0, shade=False, shade_lowest=False, legend=False, cmap="Reds").set_title(title)
+    #sns.plt.contour(xpn, ypn + n2)
+
+    image_name = "{}_{}.png".format(model_file, 100000 + state.total_steps)
+    plt.savefig(image_name)  
+  
+while True:
+  if (goal_loss <= final_goal_loss): break
+  state = train_until(goal_loss, state)
+  save_image(state)
+  goal_loss = goal_loss + loss_step
+  render_countdown = render_countdown - 1
+  if (render_countdown == 0):
+    render_countdown = 5
+    with imageio.get_writer(gif_file, mode='I', fps = 10) as writer:
+      for filename in sorted(os.listdir(".")):
+        if (model_file in filename and ".png" in filename):
+          writer.append_data(imageio.imread(filename))
