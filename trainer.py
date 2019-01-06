3d3f5665367530fdd3d6983c33d66ecdd1810561
Jomo Fisher <jomof@google.com>
2019-01-06 12:57:33 -0800

    update

--- trainer.py
+++ trainer.py
@@ -117,10 +117,10 @@ my_outfile = outputs
 x_train, x_test, y_train, y_test = train_test_split(my_infile, my_outfile, test_size=0.01)
 x_train.shape, y_train.shape, x_test.shape, y_test.shape
 
-model_file = "45-1.h5"
+model_file = "w-360-45-1.h5"
 
 my_model = keras.Sequential([
-    keras.layers.Dense(45, input_shape=(61,), kernel_initializer='normal', activation='sigmoid'),
+    keras.layers.Dense(45, input_shape=(360,), kernel_initializer='normal', activation='sigmoid'),
     #keras.layers.Dense(30, kernel_initializer='normal', activation='sigmoid'),
     keras.layers.Dense(1, kernel_initializer='normal', activation='sigmoid')
 ])
