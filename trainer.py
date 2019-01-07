ddcb3fbc334b70bcc6d41a87a98c7eb158ed260f
Jomo Fisher <jomof@google.com>
2019-01-06 21:57:11 -0800

    split

--- trainer.py
+++ trainer.py
@@ -1,119 +1,32 @@
 # TensorFlow and tf.keras
 import tensorflow as tf
 from tensorflow import keras
-import seaborn as sns
+#import seaborn as sns
 from keras.models import load_model
 # Helper libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
 import imageio
-from scipy.special import logit
-from scipy.special import expit
 from sklearn.preprocessing import scale
 from os import walk
 import os
 import matplotlib.pyplot as plt
 
 from sklearn.model_selection import train_test_split
+import featurizer as ft
 
 print(tf.__version__)
 
-
 from numpy import genfromtxt
 
-def pad_zeros(width, x):
-  return np.array(list(x) + [0] * (width - len(x)))
-
-def array_map(f, x):
-  return np.array(list(map(f, x)))
-
-def array_map_2d(f, x):
-  def ff(y):
-     return np.array(list(map(f, y)))
-  return np.array(list(map(ff, x)))
-
-def sign(a):
-  if (a > 0): return 1.0 
-  else: return 0.0
-
-def array_reverse(x):
-  l = list(x)
-  l.reverse()
-  return l
-
-def array_sign_interleave(x):
-  abses = array_map(abs, x)
-  signs = array_map(sign, x)
-  return [val for pair in zip(abses, signs) for val in pair]
-
-def remove_nans(a):
-  return [x for x in a if str(x) != 'nan']
-
 dataCsv = genfromtxt('data.csv', delimiter=',')
-
-outputs, residue = np.hsplit(dataCsv, [1])
-ratios, times = np.hsplit(residue, [1])
-outputs = np.nan_to_num(outputs)
-ratios = np.nan_to_num(ratios)
-timeWindows = times.shape[1]
-
-maxTime = max([max(map(max, times)), abs(min(map(min, times)))])
-times = times / maxTime
-
-def identity_transform(a): return a
-def reverse_transform(a): 
-  result = list(a)
-  result.reverse()
-  return result
-def descending_transform_abs(a): 
-  return sorted(list(a), key = lambda x : -abs(x))
-def ascending_transform_abs(a): 
-  return sorted(list(a), key = lambda x : abs(x))
-def descending_transform(a): 
-  return sorted(list(a), key = lambda x : -x)
-def ascending_transform(a): 
-  return sorted(list(a), key = lambda x : x)
-def backfill(width, arr):
-  x = arr.copy()
-  total = x.shape[0]
-  for i in range(0, width - 1):
-    targetIndex = total - i - 1
-    sourceIndex = width - i - 1
-    if(x[targetIndex] == 0):
-      x[targetIndex] = x[sourceIndex]
-  return x
-
-def imbed_reverse_expand_times(f):
-  def ff(a):
-    short = f(remove_nans(a))
-    return array_sign_interleave(backfill(len(short), pad_zeros(timeWindows, short)))
-  return array_map(ff, times)
-
-def expand_times(f):
-  def ff(a):
-    short = f(remove_nans(a))
-    return array_sign_interleave(pad_zeros(timeWindows, short))
-  return array_map(ff, times)
 
 def color_of_answer(answer):
   if (answer > 0.5): return 'blue'
   else: return 'gray'
 
-forwardTimes = expand_times(identity_transform)
-reverseTimes = expand_times(reverse_transform)
-descendingTimesAbs = expand_times(descending_transform_abs)
-ascendingTimesAbs = expand_times(ascending_transform_abs)
-descendingTimes = expand_times(descending_transform)
-ascendingTimes = expand_times(ascending_transform)
-
-#loss: 0.2310 - binary_accuracy: 0.8976
-my_infile = np.column_stack((forwardTimes, reverseTimes, descendingTimesAbs, ascendingTimesAbs, descendingTimes, ascendingTimes))
-#loss: 0.2180 - binary_accuracy: 0.9023 
-#my_infile = np.column_stack((ratios, forwardTimes, reverseTimes))
-#my_infile = np.column_stack((ratios, forwardTimes))
-#my_infile = ratios
-my_outfile = outputs
+my_infile, my_outfile = ft.featurize(dataCsv)
 x_train, x_test, y_train, y_test = train_test_split(my_infile, my_outfile, test_size=0.01)
 x_train.shape, y_train.shape, x_test.shape, y_test.shape
 
@@ -131,7 +44,7 @@ total_steps = 0
 x_disp = x_test
 y_disp = y_test
 tpy = np.transpose(y_disp)[0]
-c_disp = array_map(color_of_answer, tpy)
+c_disp = ft.array_map(color_of_answer, tpy)
 
 noise1 = np.random.normal(0, 0.5, y_disp.shape[0])
 noise2 = np.random.normal(0, 0.5, y_disp.shape[0])
@@ -192,7 +105,6 @@ def save_image(state):
     tpp = np.transpose(predictions)[0]
     n1 = noise1 * loss
     n2 = noise2 * loss
-    sns.set_style("dark")
     plt.clf()
     plt.ylim(-0.5, 1.5)
     plt.xlim(-0.5, 1.5)
