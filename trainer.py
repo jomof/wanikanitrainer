f80b561cc43949a11dbdc87980d98b5a82723ebe
Jomo Fisher <jomof@google.com>
2019-01-11 07:34:16 -0800

    1

--- trainer.py
+++ trainer.py
@@ -38,7 +38,7 @@ my_model = keras.Sequential([
     keras.layers.Dense(1, activation='sigmoid')
 ])
 
-optimizer = keras.optimizers.Adam(lr=0.0001, beta_1=0.9, beta_2=0.999)
+optimizer = keras.optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999)
 
 my_model.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['binary_accuracy'])
 total_epochs = 0
